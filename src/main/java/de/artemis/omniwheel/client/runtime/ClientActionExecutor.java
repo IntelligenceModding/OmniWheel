86b2eb0f522ec0ced643d01e4edf1dfa33ea39cd
Matti Gregorius <gregoriusmatti@gmail.com>
2026-09-08 22:55:49 +0200

    Updated to mod version 1.1.0 (see 1.21.1-fabric branch for more detailed changelog)

--- src/main/java/de/artemis/omniwheel/client/runtime/ClientActionExecutor.java
+++ src/main/java/de/artemis/omniwheel/client/runtime/ClientActionExecutor.java
@@ -8,6 +8,7 @@ import de.artemis.omniwheel.common.action.LocalMessageAction;
 import de.artemis.omniwheel.common.action.OpenChatAction;
 import de.artemis.omniwheel.common.action.SwitchProfileAction;
 import de.artemis.omniwheel.common.action.WheelAction;
+import de.artemis.omniwheel.common.OmniWheelText;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.network.chat.Component;
@@ -110,14 +111,16 @@ public final class ClientActionExecutor {
 
         String resolved = PlaceholderResolver.resolve(action.text(), minecraft);
         minecraft.keyboardHandler.setClipboard(resolved);
-        minecraft.player.sendOverlayMessage(Component.literal("Copied to clipboard"));
+        minecraft.player.sendOverlayMessage(OmniWheelText.component("omniwheel.message.copied_to_clipboard"));
     }
 
     private void executeSwitchProfile(SwitchProfileAction action) {
         Minecraft minecraft = Minecraft.getInstance();
         runtime().profileManager().setActiveProfile(action.profileId());
         if (minecraft.player != null) {
-            minecraft.player.sendOverlayMessage(Component.literal("Active profile: " + runtime().activeProfile().displayName()));
+            minecraft.player.sendOverlayMessage(
+                    OmniWheelText.component("omniwheel.message.active_profile", OmniWheelText.resolve(runtime().activeProfile().displayName()))
+            );
         }
     }
 
